568cc661cb07e8e420112dd7dfde2557eb3815a2
AtinDas <69185175+AtinDas@users.noreply.github.com>
2022-10-01 00:46:18 +0530

    Create Divisibility.java
    Simple JAVA program to find the divisibility

---
+++ Day_3/Divisibility.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class FizzBuzz 
+{
+	public static void main(String[] args)
+    {
+		Scanner in = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int i = in.nextInt();
+                  
+            if (i%15==0)                                                
+                System.out.println("FizzBuzz");
+            
+            else if (i%5==0)    
+                System.out.println("Buzz");
+ 
+            
+            else if (i%3==0)    
+                System.out.println("Fizz");
+                 
+            else 
+                System.out.println("number" + i + "is not divisible by 3 or 5");                        
+               
+        in.close();
+    }
+}
