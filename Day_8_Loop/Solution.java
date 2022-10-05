3d2960b967a5004eb7a379e230ab2bfc01d40f4d
AtinDas <atindas.scm@gmail.com>
2022-10-06 00:59:38 +0530

    Simple JAVA program to understand Loop concepts

---
+++ Day_8_Loop/Solution.java
@@ -0,0 +1,18 @@
+//The program is to use loops to help us do some simple math.
+//Given an integer, N , print its first 10 multiples.
+//Each multiple N x i (where 1<=i<=10 ) should be printed on a new line in the form: N x i = result.
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        
+        for (int i = 1; i <= 10; i++) {
+            System.out.printf("%d x %d = %d\n", N, i, N * i);
+        }
+    }
+}
