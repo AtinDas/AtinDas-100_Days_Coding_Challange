5fedf6a204811ad5c203ab14a7d0f8adfb8d9456
AtinDas <atindas.scm@gmail.com>
2022-12-01 21:58:23 +0530

    Simple JAVA Program on Reverse of Linked List

---
+++ Day_47_ReverseLinkedList/Reverselist.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode fakeHead = new ListNode(-1);
+        reverse(head, fakeHead);
+        return fakeHead.next;
+    }
+    //reverse returns the tail of the reversed list
+    private ListNode reverse(ListNode node, ListNode fakeHead){
+        if (node == null)
+            return fakeHead;
+        else{
+            ListNode tail = reverse(node.next, fakeHead);
+            tail.next = node;
+            node.next = null;
+            return node;
+        }
+    }
+}
